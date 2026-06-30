555ada5a4046b439dca2f981e21bfa09af650584
Camilo Layne <camilolayne4@gmail.com>
2026-06-30 21:58:01 +0900

    almost done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @response = params[:question]
+    if @response == "I am going to work"
+      then puts "Great!"
+    elsif @response.include?("?")
+      then puts "Silly question, get dressed and go to work!"
+    else puts "I don't care, get dressed and go to work!"
+    end
+  end
 end
